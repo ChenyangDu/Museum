72736577d3166219269d1479e5e664700c3b12e7
ChenyangDu <52902587+ChenyangDu@users.noreply.github.com>
2021-02-19 19:47:32 +0800

    添加灯光和天空盒子

---
+++ flydemo/Assets/PaveMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaveMove : MonoBehaviour
+{
+    public float speed = 5f;
+    float y_min;
+    float y_max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        y_min = transform.position.y;
+        y_max = y_min + 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && transform.position.y < y_max)
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        if (Input.GetMouseButton(1)&&transform.position.y>y_min)
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+    }
+}
